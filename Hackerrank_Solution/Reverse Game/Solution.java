1d729c171757246f4d8152b2633972f0245d2aa4
sakshikishore <30624981+sakshikishore@users.noreply.github.com>
2020-05-10 00:19:25 +0530

    Create Solution.java

---
+++ Hackerrank_Solution/Reverse Game/Solution.java
@@ -0,0 +1,39 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        for(int s=0;s<t;s++)
+        {
+            int n=scanner.nextInt();
+            int k=scanner.nextInt();
+            ArrayList<Integer> l=new ArrayList<Integer>();
+            int j=0;
+            for(int i=0;i<n;i=i+2)
+            {
+                l.add(n-1-j);
+                
+                if(i+1<n)
+                {
+                l.add(j);
+                }
+                j++;
+            }
+    
+        
+            System.out.println(l.indexOf(k));
+        }
+
+        scanner.close();
+    }
+}
