927a22fda262a3dd250d90a8529a68f6792108c8
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2020-06-06 13:23:18 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Height Checker/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+     int arr[]=new int[heights.length];
+        int count=0;
+        for(int i=0;i<heights.length;i++)
+        {
+            arr[i]=heights[i];
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=heights[i])
+            {
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
