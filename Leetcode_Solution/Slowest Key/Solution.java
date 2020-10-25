3e1fc24bc5fe28ea79579cb389b7672b13b1bff1
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2020-10-25 11:01:42 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Slowest Key/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        int max=0;
+        char result='a';
+        for(int i=0;i<keysPressed.length();i++)
+        {
+            if(i==0)
+            {
+                if(releaseTimes[0]>max)
+                {
+                    max=releaseTimes[0];
+                    result=keysPressed.charAt(i);
+                }
+            }
+            else
+            {
+                if(releaseTimes[i]-releaseTimes[i-1]>max)
+                {
+                    max=releaseTimes[i]-releaseTimes[i-1];
+                    result=keysPressed.charAt(i);
+                }
+                else if(releaseTimes[i]-releaseTimes[i-1]==max)
+                {
+                    if(keysPressed.charAt(i)>keysPressed.charAt(i-1))
+                    {
+                        result=keysPressed.charAt(i);
+                    }
+                }
+            }
+        }
+        
+        return result;
+        
+    }
+}
