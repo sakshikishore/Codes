69d6e788f878626100914f02b79f2ca7ddfa29fc
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2020-11-12 22:13:59 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Toeplitz Matrix/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        boolean result=true;
+        for(int i=1;i<matrix.length;i++)
+        {
+            for(int j=1;j<matrix[i].length;j++)
+            {
+                if(matrix[i][j]!=matrix[i-1][j-1])
+                {
+                    result=false;
+                    break;
+                }
+            }
+        }
+        
+        return result;
+        
+    }
+}
