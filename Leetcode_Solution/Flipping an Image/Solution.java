928dc62e7ad20fe938db2e80e50ee5a0db0d30b2
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2020-11-24 07:39:04 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Flipping an Image/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for(int i=0;i<A.length;i++)
+        {
+            for(int j=0;j<A[i].length/2;j++)
+            {
+                int temp=A[i][j];
+                A[i][j]=A[i][A[i].length-1-j];
+                A[i][A[i].length-1-j]=temp;
+                
+            }
+        }
+        
+        for(int i=0;i<A.length;i++)
+        {
+            for(int j=0;j<A[i].length;j++)
+            {
+               A[i][j]=A[i][j]^1;             
+            }
+        }
+        
+        return A;
+    }
+}
