2ec25988180c6416469519150fded1849ef186be
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2020-12-16 15:19:21 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Maximum Repeating Substring/Solution.java
@@ -0,0 +1,15 @@
+import java.util.regex.*;
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+     int count=0;
+    String str=word;
+     while(sequence.contains(str))
+     {
+         count++;
+         str=str+word;
+     }
+        
+        return count;
+        
+    }
+}
