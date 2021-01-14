6a3b0bd287f67a6758a31737675772823e1f9395
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2021-01-14 18:47:49 +0530

    Delete Solution.java

--- LeetCode_Database/Consecutive Numbers/Solution.java
+++
@@ -1,2 +0,0 @@
-# Write your MySQL query statement below
-Select distinct p2.Num As ConsecutiveNums From Logs p1, Logs p2, Logs p3 where p1.Num=p2.Num And p2.Num=p3.Num AND p2.Id-p1.Id=1 AND p3.Id-p2.Id=1 
