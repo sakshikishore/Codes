e78ebb06c410718cf3ab8574d89e2fbe096bad6d
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2021-06-13 12:12:52 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Redistribute Characters to Make All Strings Equal/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean makeEqual(String[] words) {
+        int ch[]=new int[26];
+        boolean result=true;
+        for(int i=0;i<words.length;i++)
+        {
+            for(int j=0;j<words[i].length();j++)
+            {
+                ch[words[i].charAt(j)-'a']++;
+            }
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(ch[i]%words.length!=0)
+            {
+                result=false;
+                break;
+            }
+        }
+        
+        return result;
+    }
+}
