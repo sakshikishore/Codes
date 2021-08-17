72943079a3e8358a9f5feda5e3c901e3db89e5cd
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2021-08-17 18:11:38 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Battleships in a Board/Solution.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int countBattleships(char[][] board) {
+        int count=0;
+        for(int i=0;i<board.length;i++)
+        {
+            for(int j=0;j<board[i].length;j++)
+            {
+                if(board[i][j]=='X')
+                {
+                    if(i==0)
+                    {
+                        if(j==0)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            if(board[i][j-1]!='X')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(j==0)
+                        {
+                            if(board[i-1][j]!='X')
+                            {
+                                count++;
+                            }
+                        }
+                        else
+                        {
+                            if(board[i-1][j]!='X' && board[i][j-1]!='X')
+                            {
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
