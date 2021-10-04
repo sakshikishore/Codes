0e677967a37eaae7d695a392c4ee8825f4508ff7
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2021-10-04 09:07:24 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Number of Pairs of Strings With Concatenation Equal to Target/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numOfPairs(String[] nums, String target) {
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=0;j<nums.length;j++)
+            {
+                if(i!=j)
+                {
+                    if((nums[i]+nums[j]).equals(target))
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+        
+    }
+}
