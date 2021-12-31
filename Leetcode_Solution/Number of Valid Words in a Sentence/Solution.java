f4b534934745b1cc390bbd0ccb641e823112822e
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2021-12-31 15:47:44 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Number of Valid Words in a Sentence/Solution.java
@@ -0,0 +1,45 @@
+import java.util.regex.*;
+class Solution {
+    public int countValidWords(String sentence) {
+        sentence=sentence.trim();
+        String arr[]=sentence.split("\\s+");
+        int result=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            int count=0;
+            int hyphencount=0;
+         for(int j=0;j<arr[i].length();j++)
+         {
+             if(arr[i].charAt(j)>='0' && arr[i].charAt(j)<='9')
+             {
+                 count++;
+                 break;
+             }
+             if(arr[i].charAt(j)=='-')
+             {
+                 if( hyphencount==0 && j>0 && arr[i].charAt(j-1)>='a' && arr[i].charAt(j-1)<='z' && j<arr[i].length()-1 && arr[i].charAt(j+1)>='a' && arr[i].charAt(j+1)<='z')
+                 {
+                     hyphencount=1;
+                 }
+                 else
+                 {
+                     count++;
+                     break;
+                 }
+             }
+             if((arr[i].charAt(j)=='!' || arr[i].charAt(j)=='.' || arr[i].charAt(j)==',') && j!=arr[i].length()-1)
+             {
+                count++;
+                 break;
+             }
+         }
+            if(count==0)
+            {
+                result++;
+            }
+        }
+        
+        return result;
+        
+    }
+}
