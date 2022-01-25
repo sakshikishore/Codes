2dae0cdc1a28b0a67f55d0132c642e37917f3191
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-01-25 11:14:23 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Count Elements With Strictly Smaller and Greater Elements/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countElements(int[] nums) {
+        int count=0;
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length-1;i++)
+        {
+            if(nums[i]>nums[0] && nums[i]<nums[nums.length-1])
+            {
+                count++;
+            }
+        }
+        
+        return count;
+        
+    }
+}
