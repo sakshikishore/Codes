cfd58a065eede22aa43b4bf798166a1a5b105f71
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-02-23 10:19:17 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Airplane Seat Assignment Probability/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double nthPersonGetsNthSeat(int n) {
+        double result=0.0;
+        if(n==1)
+        {
+            result=1;
+        }
+        else
+        {
+            result=1/2.0;
+        }
+        
+        return result;
+        
+    }
+}
