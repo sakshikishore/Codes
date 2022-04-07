be0a83d65872f3256fb27a57899ebf16091a24c1
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-04-07 10:11:27 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Find Players With Zero or One Losses/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        int winner[]=new int[100001];
+        int looser[]=new int[100001];
+        List<List<Integer>> list=new ArrayList();
+        ArrayList<Integer> l=new ArrayList<Integer>();
+        ArrayList<Integer> m=new ArrayList<Integer>();
+        for(int i=0;i<matches.length;i++)
+        {
+            winner[matches[i][0]]++;
+            looser[matches[i][1]]++;
+        }
+        
+        for(int i=0;i<100001;i++)
+        {
+            if(winner[i]>0 && looser[i]==0)
+            {
+                l.add(i);
+            }
+            if(looser[i]==1)
+            {
+                m.add(i);
+            }
+        }
+        
+        list.add(l);
+        list.add(m);
+        
+        return list;
+        
+    }
+}
