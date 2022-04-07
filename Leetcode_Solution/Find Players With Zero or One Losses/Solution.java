c7051567bd52f9137c540cd32e022c9742df86a8
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-04-07 10:18:19 +0530

    Update Solution.java

--- Leetcode_Solution/Find Players With Zero or One Losses/Solution.java
+++ Leetcode_Solution/Find Players With Zero or One Losses/Solution.java
@@ -11,7 +11,7 @@ class Solution {
             looser[matches[i][1]]++;
         }
         
-        for(int i=0;i<100001;i++)
+        for(int i=1;i<100001;i++)
         {
             if(winner[i]>0 && looser[i]==0)
             {
