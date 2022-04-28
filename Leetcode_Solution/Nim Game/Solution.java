62957f4523245823a930959e524890470c75f03f
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-04-28 10:35:38 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Nim Game/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        boolean result=true;
+        if(n%4==0)
+        {
+            result=false;
+        }
+        
+        return result;
+    }
+}
