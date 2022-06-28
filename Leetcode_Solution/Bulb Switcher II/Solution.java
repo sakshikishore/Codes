720b29a21f6948122e140e038c8943c5faf546cc
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-06-28 12:17:51 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Bulb Switcher II/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int flipLights(int n, int presses) {
+        if(presses==0)
+        {
+            return 1;
+        }
+       else if(n==1)
+        {
+           return 2;
+        }
+        else if(n==2)
+        {
+            if(presses==1)
+            {
+                return 3;
+            }
+            else
+            {
+                return 4;
+            }
+        }
+        else
+        {
+            if(presses==1)
+            {
+                return 4;
+            }
+            else if(presses==2)
+            {
+                return 7;
+            }
+            else
+            {
+                return 8;
+            }
+        }
+        
+    }
+}
