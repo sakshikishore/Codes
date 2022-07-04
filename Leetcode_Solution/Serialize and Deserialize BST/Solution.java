7d8079618726a8d220dac07002d8d04c9d7a5f6f
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-07-04 10:31:55 +0530

    Update Solution.java

--- Leetcode_Solution/Serialize and Deserialize BST/Solution.java
+++ Leetcode_Solution/Serialize and Deserialize BST/Solution.java
@@ -1,12 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 public class Codec {
     
     // Encodes a tree to a single string.
