6a04b59357e8b6d75ce927fe5c8c151129d40c1c
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-08-09 15:31:24 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Reorganize String/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String reorganizeString(String s) {
+        int ch[]=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            ch[s.charAt(i)-'a']++;
+        }
+        int curr=-1;
+        String str="";
+        char c='a';
+        for(int i=0;i<s.length();i++)
+        {
+            int max=0;
+            int flag=0;
+            int p=curr;
+            for(int j=0;j<26;j++)
+            {
+                if(ch[j]>max && j!=curr)
+                {
+                    flag=1;
+                    c=(char)(97+j);
+                    p=j;
+                    max=ch[j];
+                }
+            }
+            if(flag==0)
+            {
+                break;
+            }
+            str=str+Character.toString(c);
+            ch[p]--;
+            curr=p;
+
+        }
+        if(str.length()!=s.length())
+        {
+            str="";
+        }
+        
+        return str;
+    }
+}
