08bf9638b64db7c36181b35d1bc0bef7f5f8d6a7
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-08-20 07:59:51 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Splitting a String Into Descending Consecutive Values/Solution.java
@@ -0,0 +1,72 @@
+import java.math.BigInteger;
+class Solution {
+    public boolean splitString(String s) {
+        boolean result=false;
+        List<List<String>> list=new ArrayList();
+
+        for(int i=0;i<s.length();i++)
+        {
+            ArrayList<String> l=new ArrayList<String>();
+            list.add(l);
+        }
+        
+        for(int i=0;i<s.length();i++)
+        {
+            String str=s.substring(0,i+1);
+            if(i==0)
+            {
+                list.get(i).add(str);
+            }
+            else
+            {
+                list.get(i).add(str);
+                for(int j=i;j>=1;j--)
+                {
+                     String p=s.substring(j,str.length());
+                     BigInteger x=new BigInteger(p);
+                    for(int k=0;k<list.get(j-1).size();k++)
+                    {
+                        String a=list.get(j-1).get(k);
+                        String z[]=a.split(" ");
+                        BigInteger y=new BigInteger(z[z.length-1]);
+                        BigInteger sub=y.subtract(x);
+                        if(sub.equals(BigInteger.valueOf(1)))
+                        {
+                             a=a+" "+p;
+                              list.get(i).add(a);
+                        }
+                    }
+                }
+            }
+        }
+        int n=s.length()-1;
+        for(int x=0;x<list.get(n).size();x++)
+        {
+            String str[]=list.get(n).get(x).split(" ");
+            if(str.length>1)
+            {
+                long arr[]=new long[str.length];
+                for(int i=0;i<str.length;i++)
+                {
+                    arr[i]=Long.parseLong(str[i]);
+                }
+                int flag=0;
+                for(int i=1;i<str.length;i++)
+                {
+                   if(arr[i-1]-arr[i]!=1)
+                   {
+                       flag=1;
+                       break;
+                   }
+                }
+                if(flag==0)
+                {
+                    result=true;
+                    break;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
