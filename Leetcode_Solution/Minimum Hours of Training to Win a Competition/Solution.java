6ee9df42320db3b897be2c6f63a2e051f0240dfa
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-08-21 12:20:44 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Minimum Hours of Training to Win a Competition/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
+        int available_energy=initialEnergy;
+        int available_experience=initialExperience;
+        int count=0;
+        for(int i=0;i<energy.length;i++)
+        {
+            if(energy[i]<available_energy && available_experience>experience[i])
+            {
+                available_energy=available_energy-energy[i];
+                available_experience+=experience[i];
+                continue;
+            }
+            else
+            {
+                if(energy[i]>=available_energy)
+                {
+                    count+=(1+energy[i])-available_energy;
+                    available_energy=1;
+                }
+                else
+                {
+                    available_energy=available_energy-energy[i];
+                }
+                if(experience[i]>=available_experience)
+                {
+                    int p=(1+experience[i])-available_experience;
+                    available_experience+=p;
+                    available_experience+=experience[i];
+                    count+=p;
+                }
+                else
+                {
+                    available_experience+=experience[i];
+                }
+            }
+            System.out.println(available_experience+" "+available_energy);
+        }
+        
+        return count;
+    }
+}
