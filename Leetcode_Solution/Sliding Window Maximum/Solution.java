88a67863d353194c61e9d646b5fbf39f762635ce
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-09-14 05:37:09 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Sliding Window Maximum/Solution.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int j=0;
+        int max=0;
+        int result[]=new int[nums.length-k+1];
+        Deque<Integer> dq=new LinkedList<Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i<k)
+            {
+                if(dq.size()==0)
+                {
+                    dq.add(nums[i]);
+                }
+                else if(dq.getLast()<nums[i])
+                {
+                    dq.pollLast();
+                    while(dq.size()>0 && dq.getLast()<nums[i])
+                    {
+                        dq.pollLast();
+                    }
+                    dq.add(nums[i]);
+                }
+                else
+                {
+                    dq.add(nums[i]);
+                }
+            }
+            else
+            {
+                result[j]=dq.getFirst();
+                if(dq.getFirst()==nums[j])
+                {
+                   dq.poll(); 
+                }
+                j++;
+                if(dq.size()==0)
+                {
+                    dq.add(nums[i]);
+                }
+                else if(dq.getLast()<nums[i])
+                {
+                    dq.pollLast();
+                    while(dq.size()>0 && dq.getLast()<nums[i])
+                    {
+                        dq.pollLast();
+                    }
+                    dq.add(nums[i]);
+                }
+                else
+                {
+                    dq.add(nums[i]);
+                }
+                
+            }
+        }
+        
+        result[j]=dq.getFirst();
+        
+        return result;
+        
+        
+    }
+}
