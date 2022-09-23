3a69907964c5229c44d0f3e6eee0c559f1a54ef0
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-09-23 07:29:05 +0530

    Create Solution.java

---
+++ Leetcode_Solution/Minimum Limit of Balls in a Bag/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumSize(int[] nums, int maxOperations) {
+        int result=0;
+        Arrays.sort(nums);
+        int max=nums[nums.length-1];
+        int min=1;
+        while(min<=max)
+        {
+            int mid=(max+min)/2;
+            double sum=0;
+            int flag=0;
+            for(int j=0;j<nums.length;j++)
+            {
+              if(nums[j]>mid)
+              {
+                  double m=mid;
+                  sum+=Math.ceil(nums[j]/m);
+                  sum=sum-1;
+                  if(sum>maxOperations)
+                  {
+                      flag=1;
+                      break;
+                  }
+              }
+            }
+            if(flag==0)
+            {
+                result=mid;
+                max=mid-1;
+            }
+            else
+            {
+                min=mid+1;
+            }
+        }
+        
+        return result;
+    }
+}
