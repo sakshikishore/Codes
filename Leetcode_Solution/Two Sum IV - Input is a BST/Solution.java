89f78eb9025291b5b0c6fe8e27b20aaf8e7e6cc9
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2022-10-09 07:54:07 +0530

    Update Solution.java

--- Leetcode_Solution/Two Sum IV - Input is a BST/Solution.java
+++ Leetcode_Solution/Two Sum IV - Input is a BST/Solution.java
@@ -1,18 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     ArrayList<Integer> l=new ArrayList<Integer>();
     public int height(TreeNode root)
