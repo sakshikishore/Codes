dbe371434e33cf8a25ef52989205f5610c08231c
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2023-01-15 15:27:58 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Maximum Count of Positive Integer and Negative Integer/Solution.java
@@ -0,0 +1,20 @@
+lass Solution {
+    public int maximumCount(int[] nums) {
+        int pos=0,neg=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]<0)
+            {
+                neg++;
+            }
+            else if(nums[i]>0)
+            {
+                pos=nums.length-i;
+                break;
+            }
+        }
+        
+        return Math.max(pos,neg);
+
+    }
+}
