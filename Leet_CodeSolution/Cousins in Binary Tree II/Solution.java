e84c917a957a663084982800676b2f5572b81dc1
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2023-04-16 11:17:29 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Cousins in Binary Tree II/Solution.java
@@ -0,0 +1,107 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode replaceValueInTree(TreeNode root) {
+        Queue<TreeNode> q=new LinkedList<TreeNode>();
+        q.add(root);
+        q.add(null);
+        ArrayList<Integer> l=new ArrayList<Integer>();
+        int sum=0;
+        while(q.size()>0)
+        {
+            TreeNode temp=q.poll();
+            if(temp!=null)
+            {
+                sum+=temp.val;
+                if(temp.left!=null)
+                {
+                    q.add(temp.left);
+                }
+                if(temp.right!=null)
+                {
+                    q.add(temp.right);
+                }
+            }
+            else
+            {
+                l.add(sum);
+                System.out.println(sum);
+                if(q.size()>0)
+                {
+                    q.add(null);
+                    sum=0;
+                }
+            }
+        }
+        q=new LinkedList<TreeNode>();
+        q.add(root);
+        q.add(null);
+        int i=0;
+        while(q.size()>0)
+        {
+            TreeNode temp=q.poll();
+            if(temp!=null)
+            {
+                if(i==0 || i==1)
+                {
+                    temp.val=0;
+                }
+                if(temp.left!=null && temp.right!=null)
+                {
+                     if(i!=0)
+                     {
+                         sum=l.get(i+1);
+                         int p=temp.left.val+temp.right.val;
+                         temp.left.val=sum-p;
+                         temp.right.val=sum-p;
+                     }
+                    q.add(temp.left);
+                    q.add(temp.right);
+                }
+                else if(temp.left!=null)
+                {
+                    if(i!=0)
+                     {
+                         sum=l.get(i+1);
+                         int p=temp.left.val;
+                         temp.left.val=sum-p;
+                     }
+                    q.add(temp.left);
+                }
+                else if(temp.right!=null)
+                {
+                    if(i!=0)
+                     {
+                         sum=l.get(i+1);
+                         int p=temp.right.val;
+                         temp.right.val=sum-p;
+                     }
+                    q.add(temp.right);
+                }
+            }
+            else
+            {
+                if(q.size()!=0)
+                {
+                    q.add(null);
+                    i++;
+                }
+            }
+        }
+        
+        return root;
+    }
+}
