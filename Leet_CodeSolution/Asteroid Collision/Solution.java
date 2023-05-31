e1259ba341b3e86ef21de05219447db1227bb8ee
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2023-05-31 11:25:52 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Asteroid Collision/Solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stck=new Stack<Integer>();
+        for(int i=0;i<asteroids.length;i++)
+        {
+            if(stck.empty())
+            {
+                stck.push(asteroids[i]);
+            }
+            else
+            {
+                if((stck.peek()>0 && asteroids[i]<0))
+                {
+                     int p=Math.abs(stck.peek());
+                     int q=Math.abs(asteroids[i]);
+                     if(p==q)
+                     {
+                         stck.pop();
+                     }
+                     else if(p<q)
+                     {
+                         stck.pop();
+                         i--;
+                     }
+                }
+                else
+                {
+                    stck.push(asteroids[i]);
+                }
+            }
+        }
+
+        int result[]=new int[stck.size()];
+        int i=stck.size()-1;
+        while(stck.size()>0)
+        {
+           result[i]=stck.pop();
+           i--;
+        }
+
+
+        return result;
+
+    }
+}
