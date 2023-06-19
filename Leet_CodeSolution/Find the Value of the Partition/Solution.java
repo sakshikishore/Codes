9e6f453b3a9edd3b8ece5561ca4d713a6ba719f8
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2023-06-19 05:38:15 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Find the Value of the Partition/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findValueOfPartition(int[] nums) {
+        Arrays.sort(nums);
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]-nums[i-1]<min)
+            {
+                min=nums[i]-nums[i-1];
+            }
+        }
+        
+        return min;
+    }
+}
