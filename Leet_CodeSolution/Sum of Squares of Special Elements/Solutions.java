a7414960e89c51d7bc9aace3659ea9183a2d66e4
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2023-07-17 06:34:07 +0530

    Create Solutions.java

---
+++ Leet_CodeSolution/Sum of Squares of Special Elements/Solutions.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int n=nums.length;
+        int result=0;
+        for(int i=1;i<=nums.length;i++)
+        {
+            if(n%i==0)
+            {
+                result=result+(nums[i-1]*nums[i-1]);
+            }
+        }
+        
+        return result;
+        
+    }
+}
