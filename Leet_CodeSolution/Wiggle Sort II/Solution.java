9815b30a85bc1c7bc65e20b6540baabbc22f774a
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2023-08-19 07:01:00 +0530

    Update Solution.java

--- Leet_CodeSolution/Wiggle Sort II/Solution.java
+++ Leet_CodeSolution/Wiggle Sort II/Solution.java
@@ -154,7 +154,6 @@ class Solution {
                     }
                     else
                     {
-                        System.out.println("sakshi");
                          for(int j=i-2;j>=0;j--)
                         {
                             if(nums[j]<nums[i])
