98c9f057b0957a2fcaa3253c674da3c255e938e0
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2023-10-12 08:49:13 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Frequency of the Most Frequent Element/Solution.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        long p=k,max=0,count=1;
+        Arrays.sort(nums);
+        int start=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==nums[i-1])
+            {
+                count++;
+            }
+            else
+            {
+                 int j=nums[i]-nums[i-1];
+                 if(count*j<=p)
+                 {
+                      p=p-(count*j);
+                      count++;
+                 }
+                 else
+                 {
+                     if(count>max)
+                     {
+                         max=count;
+                     }
+                     while(start<i-1 && count*j>p)
+                     {
+                        p=p+(nums[i-1]-nums[start]);
+                        count--;
+                        start++;
+                     }
+                     if(count*j<=p)
+                     {
+                            p=p-(count*j);
+                            count++;
+                     }
+                     else
+                     {
+                         count=1;
+                         p=k;
+                     }
+
+                 }
+            }
+        }
+
+        if(count>max)
+        {
+            System.out.println(nums[nums.length-1]+" "+p);
+            max=count;
+        }
+
+        return (int)max;
+    }
+}
