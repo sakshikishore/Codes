756e6e4c2cc307ee4f2ffd2b6a100aacee32ebcb
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2023-10-29 07:56:30 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Minimum Number of Changes to Make Binary String Beautiful/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minChanges(String s) {
+        int count=0;
+        for(int i=1;i<s.length();i=i+2)
+        {
+            if(s.charAt(i)!=s.charAt(i-1))
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
