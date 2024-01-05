1cf62aacd389435778eb865e55785d71ab9f4931
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2024-01-05 08:31:24 +0530

    Update Solution.java

--- Leet_CodeSolution/Path with Maximum Gold/Solution.java
+++ Leet_CodeSolution/Path with Maximum Gold/Solution.java
@@ -21,7 +21,6 @@ class Solution {
 
     }
     public int getMaximumGold(int[][] grid) {
-        int maxGold=0;
         for(int i=0;i<grid.length;i++)
         {
            for(int j=0;j<grid[0].length;j++)
