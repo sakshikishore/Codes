703da08dcc678148dd7b68469e3fe450a040ccc3
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2024-01-20 11:15:38 +0530

    Update Solution.java

--- Leet_CodeSolution/Sum of Subarray Minimums/Solution.java
+++ Leet_CodeSolution/Sum of Subarray Minimums/Solution.java
@@ -1,23 +1,44 @@
+class Node
+{
+    int index;
+    int val;
+    public Node(int index, int val)
+    {
+        this.index=index;
+        this.val=val;
+    }
+}
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        long result=0;
-        for(int i=0;i<arr.length;i++)
+        Stack<Node> stck=new Stack<Node>();
+        Node node=new Node(arr.length-1,arr[arr.length-1]);
+        stck.push(node);
+        long ans=0;
+        for(int i=arr.length-2;i>=0;i--)
         {
-            int min=arr[i];
-            result+=min;
-            for(int j=i+1;j<arr.length;j++)
-            {
-                 if(arr[j]<min)
-                 {
-                     min=arr[j];
-                 }
-                 result+=min;
-            }
-
-            result=result%1000000007;
-
+           while(!stck.empty() && arr[stck.peek().index]>arr[i])
+           {
+               
+               ans=ans+stck.pop().val;
+               ans=ans%(1000000007);
+           }
+           if(stck.empty())
+           {
+               node=new Node(i,(arr.length-i)*arr[i]);
+               stck.push(node);
+           }
+           else
+           {
+               node=new Node(i,(stck.peek().index-i)*arr[i]+stck.peek().val);
+               stck.push(node); 
+           }
+        }
+        while(!stck.empty())
+        {
+              ans+=stck.pop().val;
+              ans=ans%(1000000007);
         }
 
-        return (int)result%1000000007;
+        return (int)ans;
     }
 }
