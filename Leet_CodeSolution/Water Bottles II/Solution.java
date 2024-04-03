8ee65de80d23075611dde62c8a1373eaf2c7e2a9
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2024-04-03 05:57:44 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Water Bottles II/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans=numBottles;
+        while(numBottles>=numExchange)
+        {
+            ans++;
+            numBottles=numBottles-numExchange+1;
+            numExchange++;
+        }
+        
+        return ans;
+        
+    }
+}
