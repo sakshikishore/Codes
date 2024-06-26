b80ecac7a97b0907ee0627e1c807dee6ae68d377
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2024-06-26 17:18:30 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Find Minimum Operations to Make All Elements Divisible by Three/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]%3!=0)
+            {
+                sum+=1;
+            }
+        }
+        
+        return sum;
+    }
+}
