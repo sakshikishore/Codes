7d9a1aea6cb4c609c76d45afdabb0a9b28475a1a
Sakshi Kishore <30624981+sakshikishore@users.noreply.github.com>
2024-06-26 17:19:34 +0530

    Create Solution.java

---
+++ Leet_CodeSolution/Minimum Operations to Make Binary Array Elements Equal to One II/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(count%2==0)
+            {
+                if(nums[i]==0)
+                {
+                    count++;
+                }
+            }
+            else
+            {
+                if(nums[i]==1)
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
